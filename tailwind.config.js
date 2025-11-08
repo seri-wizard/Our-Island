70ffbcfe2291cb5f2dec7de45c7296b1d6306313
seri-wizard <liam@socialenterprise.ie>
2025-11-08 16:10:03 +0000

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: ["./app/**/*.{js,jsx,ts,tsx}"],
+// CommonJS (safe default for Next)
+module.exports = {
+  content: [
+    "./app/**/*.{js,jsx,ts,tsx}",
+    "./components/**/*.{js,jsx,ts,tsx}",
+  ],
   theme: { extend: {} },
   plugins: [],
 };
